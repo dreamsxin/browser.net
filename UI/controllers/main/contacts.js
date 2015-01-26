2179dceefa575836f1f4ee93a9a1fd0eb0bf57fa
朱宗鑫 <176013762@qq.com>
2015-01-26 13:56:41 +0800

    Updated contacts

--- UI/controllers/main/contacts.js
+++ UI/controllers/main/contacts.js
@@ -4,6 +4,7 @@ define([
 ], function (Ember, Eotu) {
 	return Ember.ObjectController.extend({
 		contact: null,
+		messages: Ember.A([]),
 		actions: {
 			select: function (contact) {
 				Eotu.Contacts.forEach(function (item) {
@@ -11,6 +12,7 @@ define([
 				});
 				contact.set("isSelected", true);
 				this.set('contact', contact);
+				this.messages.pushObject(Ember.Object.extend({from:'郑依丽', message:'测试', date:(new Date()).toLocaleString()}).create());
 			}
 		}
 	});
